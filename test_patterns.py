6066ff299c44a2f428394f5f23a636e716a6afe3
Suor <suor.web@gmail.com>
2013-05-21 17:36:14 +0800

    Added factorial() test

--- test_patterns.py
+++ test_patterns.py
@@ -162,16 +162,16 @@ def test_tail_destruct():
     assert tail_destruct((1,'X',2)) == 'tXt'
 
 
-# def test_factorial():
-#     @patterns
-#     def factorial():
-#         if 0: 1
-#         if n is int: n * factorial(n-1)
-#         if []: []
-#         if [x] + xs: [factorial(x)] + factorial(xs)
-#         if {'n': n, 'f': f}: f(factorial(n))
-#
-#     assert factorial(0) == 1
-#     assert factorial(5) == 120
-#     assert factorial([3,4,2]) == [6, 24, 2]
-#     assert factorial({'n': [5, 1], 'f': sum}) == 121
+def test_factorial():
+    @patterns
+    def factorial():
+        if 0: 1
+        if n is int: n * factorial(n-1)
+        if []: []
+        if [x] + xs: [factorial(x)] + factorial(xs)
+        if {'n': n, 'f': f}: f(factorial(n))
+
+    assert factorial(0) == 1
+    assert factorial(5) == 120
+    assert factorial([3,4,2]) == [6, 24, 2]
+    assert factorial({'n': [5, 1], 'f': sum}) == 121
