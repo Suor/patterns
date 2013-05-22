4cd5338ed4dfc556d395f727b1fcb14687000fe8
Suor <suor.web@gmail.com>
2013-05-22 17:01:15 +0800

    Upped version to 0.1.1, updated CHANGELOG

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='patterns',
-    version='0.1.0',
+    version='0.1.1',
     author='Alexander Schepanovski',
     author_email='suor.web@gmail.com',
 
