7f6a8ae9f3c3d2dfc028733afb5ab796871ffae7
Suor <suor.web@gmail.com>
2013-05-22 16:52:58 +0800

    Prepared initial release

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='patterns',
+    version='0.1.0',
+    author='Alexander Schepanovski',
+    author_email='suor.web@gmail.com',
+
+    description='Pattern matching for python',
+    long_description=open('README.rst').read(),
+    url='http://github.com/Suor/patterns',
+    license='BSD',
+
+    packages=['patterns'],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Pre-processors',
+        'Intended Audience :: Developers',
+    ]
+)
